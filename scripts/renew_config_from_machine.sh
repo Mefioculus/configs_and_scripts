34c2346c0aef88c709c12470822d8439e370c7a5
Mefioculus <lin0ge@yandex.ru>
2021-11-29 16:28:07 +0300

    Создание скрипта для обновления конфигов, находящихся в репозитории с конфигов машины, откуда скрипт запущен

---
+++ scripts/renew_config_from_machine.sh
@@ -0,0 +1,16 @@
+# Производим поиск конфигурации nvim
+if [ -e $HOME/.comfig/nvim/init.vim ]
+then
+    echo "Конфиг nvim на этой машине найден. Будет произведено его копирование в репозиторий"
+    cp $HOME/.config/nvim/init.vim ./configs/init.vim
+else
+    echo "Конфиг nvim на данной машине не был найден"
+fi
+
+# Производим поиск конфигурации vim
+if [ -e /usr/share/vim/vimrc ]
+then
+    echo "Конфиг vim на этой машине найден. Будет произведено его копирование в репозиторий"
+    cp /usr/share/vim/vimrc ./configs/init.vim
+else
+    echo "Конфиг vim на данной машине не был найден"
