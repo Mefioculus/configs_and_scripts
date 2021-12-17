6efa214548b1716718f3f1f9922db52dbe406512
Mefioculus <lin0ge@yandex.ru>
2021-12-17 23:50:36 +0300

    Исправление ошибок в скрипте копирования файлов конфигов

--- scripts/renew_config_from_machine.sh
+++ scripts/renew_config_from_machine.sh
@@ -1,8 +1,8 @@
 # Производим поиск конфигурации nvim
-if [ -e $HOME/.comfig/nvim/init.vim ]
+if [ -e $HOME/.config/nvim/init.vim ]
 then
     echo "Конфиг nvim на этой машине найден. Будет произведено его копирование в репозиторий"
-    cp $HOME/.config/nvim/init.vim ./configs/init.vim
+    cp $HOME/.config/nvim/init.vim ../configs/init.vim
 else
     echo "Конфиг nvim на данной машине не был найден"
 fi
@@ -11,6 +11,7 @@ fi
 if [ -e /usr/share/vim/vimrc ]
 then
     echo "Конфиг vim на этой машине найден. Будет произведено его копирование в репозиторий"
-    cp /usr/share/vim/vimrc ./configs/init.vim
+    cp /usr/share/vim/vimrc ../configs/vimrc
 else
     echo "Конфиг vim на данной машине не был найден"
+fi
