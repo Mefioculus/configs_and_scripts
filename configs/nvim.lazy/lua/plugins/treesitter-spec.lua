5da74384cb4f2ba41f1aa0af12d0aa78d8e84055
Mefioculus <lin0ge@yandex.ru>
2024-07-25 16:35:45 +0300

    Исправление конфигурации Treesitter, из-за которой некорректно работал concealer, а так же подсветка синтаксиса

--- configs/nvim.lazy/lua/plugins/treesitter-spec.lua
+++ configs/nvim.lazy/lua/plugins/treesitter-spec.lua
@@ -1,36 +1,34 @@
-local M = {
+return {
     'nvim-treesitter/nvim-treesitter',
     dependencies = { 'nvim-lua/plenary.nvim' },
-    opts = {
-        highlight = {
-            enable = true,
-            custom_captures = {
-                additional_vim_regex_highlighting = false,
+    config = function()
+        require('nvim-treesitter.configs').setup {
+            highlight = {
+                enable = true,
+                custom_captures = {
+                    additional_vim_regex_highlighting = false,
+                },
             },
-        },
-        ensure_installed = {
-            "bash",
-            "c_sharp",
-            "xml",
-            "json",
-            "html",
-            "python",
-            "rust",
-            "markdown",
-            "markdown-inline",
-            "norg-meta",
-            "norg",
-            "regex",
-            "toml",
-            "yaml",
-            "vim",
-            "vimdoc",
-            "diff",
-        },
-    },
+            ensure_installed = {
+                "bash",
+                "c_sharp",
+                "xml",
+                "json",
+                "html",
+                "python",
+                "rust",
+                "markdown",
+                "norg",
+                "regex",
+                "toml",
+                "yaml",
+                "vim",
+                "vimdoc",
+                "diff",
+            },
+        }
+
+        vim.opt.foldmethod = "expr"
+        vim.opt.foldexpr = "nvim_treesitter#foldexpr()"
+        end,
 }
-
-vim.opt.foldmethod = "expr"
-vim.opt.foldexpr = "nvim_treesitter#foldexpr()"
-
-return M
