605346952bb02d3759b8fcd1700c21cc2c083435
Mefioculus <lin0ge@yandex.ru>
2024-09-19 10:46:55 +0300

    Небольшая корректировка настроек, добавление нового плагина

--- configs/nvim.lazy/lua/config/lazy.lua
+++ configs/nvim.lazy/lua/config/lazy.lua
@@ -33,5 +33,5 @@
       -- colorscheme that will be used when installing plugins.
       install = { colorscheme = { "habamax" } },
       -- automatically check for plugin updates
-      checker = { enabled = true },
+      checker = { enabled = false },
     })
