76eb8b86106932f3890610cc27fccc0658bdac1e
Mefioculus <lin0ge@yandex.ru>
2025-12-01 14:08:32 +0300

    Наведение порядка в конфигах

--- configs/nvim.lazy/lua/plugins/lspconfig-spec.lua
+++ configs/nvim.lazy/lua/plugins/lspconfig-spec.lua
@@ -2,5 +2,9 @@ return {
     {
         'neovim/nvim-lspconfig',
         lazy = false,
+        --config = function()
+        --    local lspconfig = require("lspconfig")
+        --    lspconfig.roslyn.setup({})
+        --end,
     }
 }
